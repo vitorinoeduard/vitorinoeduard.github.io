9bb549c217d8ef4aece30c2d6d39244d0e2cf563
vitorinoeduard <eduardo344vitorino@gmail.com>
2026-09-20 20:04:12 -0300

    Adiciona lootcave_img e atualiza loot-card

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// --- CONT DE IDIOMA DO MANIFESTO ---
+// --- CONT DE IDMA---
+
 const btnToggle = document.getElementById('lang-toggle');
 const textPT = document.getElementById('manifesto-pt');
 const textEN = document.getElementById('manifesto-en');
@@ -16,21 +17,22 @@ if (btnToggle && textPT && textEN) {
   });
 }
 
-// --- CONTROLE DA CAVERNA DO LOOT ---
+// --- CONT CAVE ---
+
 const btnLoot = document.getElementById('btn-loot');
 const lootLoading = document.getElementById('loot-loading');
 const lootGrid = document.getElementById('loot-grid');
 
 if (btnLoot && lootLoading && lootGrid) {
   btnLoot.addEventListener('click', () => {
-    // 1. Exibe a mensagem de carregamento e esconde o grid
+    
     lootLoading.classList.remove('hidden');
     lootGrid.classList.add('hidden');
     btnLoot.disabled = true;
 
-    // 2. Simula o tempo de descriptografia (1 segundo)
+    
     setTimeout(() => {
-      // 3. Esconde o carregamento e revela os cards
+     
       lootLoading.classList.add('hidden');
       lootGrid.classList.remove('hidden');
       btnLoot.disabled = false;
